280b84ec68b27ad1dc2fab24167f9bee38dd17bd
leenballan12-lab <lineballan12@gmail.com>
2026-06-16 15:21:03 +0200

    fix uploads

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 
 
@@ -1344,12 +1344,12 @@ app.put("/invoices/:id", (req, res) => {
   );
 
 });
-
 const multer = require("multer");
+
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, "uploads/"); // 👈 هون
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
@@ -1357,7 +1357,6 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
-
 app.post(
   "/requests-upload",
   upload.fields([
