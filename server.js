62762d3f22b34f9dcee66fa91eced135beddd1d9
leenballan12-lab <lineballan12@gmail.com>
2026-06-16 14:22:07 +0200

    fix app initialization order

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require("express");
 
 const cors = require("cors");
 const path = require("path");
-app.use("/uploads", express.static("uploads"));
+
 
 
 
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use("/uploads", express.static("uploads"));
 
 
 
